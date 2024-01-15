6b799086d9c1148cc5d33e83f20864e7c9a44e84
TrungLam <huynhtrunglam167@gmail.com>
2024-01-15 17:50:42 +0700

    Initial commit

---
+++ app/src/main/java/com/aplication/appfoody/Activity/IntroActivity.java
@@ -0,0 +1,41 @@
+package com.aplication.appfoody.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.aplication.appfoody.R;
+import com.aplication.appfoody.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BaseActivity {
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        setVariable();
+        getWindow().setStatusBarColor(Color.parseColor("#FFE4B5"));
+    }
+
+    private void setVariable() {
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        binding.signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
