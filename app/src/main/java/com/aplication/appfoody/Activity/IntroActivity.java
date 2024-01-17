455f4b71774fb2debc06d189a14b5d497afcdc4a
TrungLam <huynhtrunglam167@gmail.com>
2024-01-17 13:31:07 +0700

    update signup and login

--- app/src/main/java/com/aplication/appfoody/Activity/IntroActivity.java
+++ app/src/main/java/com/aplication/appfoody/Activity/IntroActivity.java
@@ -2,6 +2,7 @@ package com.aplication.appfoody.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -27,13 +28,17 @@ public class IntroActivity extends BaseActivity {
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(mAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                } else {
+                    startActivity(new Intent(IntroActivity.this , LoginActivity.class));
+                }
             }
         });
         binding.signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(IntroActivity.this, SignupActivity.class));
             }
         });
     }
