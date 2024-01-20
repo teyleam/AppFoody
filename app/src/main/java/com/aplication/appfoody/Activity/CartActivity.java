963fd8e10e8583cb695e7f1d7299e5f6e4381781
TrungLam <huynhtrunglam167@gmail.com>
2024-01-20 21:34:35 +0700

    UI Cart

---
+++ app/src/main/java/com/aplication/appfoody/Activity/CartActivity.java
@@ -0,0 +1,16 @@
+package com.aplication.appfoody.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aplication.appfoody.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
